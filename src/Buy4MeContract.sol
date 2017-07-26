1d98000a079c09962434f4e28913fbae72ea3067
avergnaud <a.vergnaud@gmail.com>
2017-07-26 21:40:13 +0200

    proposition de contrat - double séquestre

--- src/Buy4MeContract.sol
+++ src/Buy4MeContract.sol
@@ -1,16 +1,31 @@
 pragma solidity ^0.4.0;
 
-//TODO KO + incomplet
+/*
+http://solidity.readthedocs.io/en/develop/security-considerations.html
+https://github.com/ConsenSys/smart-contract-best-practices
+TODO v2 : uploader la preuve d'achat dans la blockchain
+*/
 contract Buy4MeContract {
+
+  /* commission en wei */
+  uint commissionBuy4Me;
+  /* pénalité en cas d'annulation pour l'acheteur final en wei */
+  uint penaliteAnnulationAcheteurFinal;
+  /* caution miniamle pour le prestataire en wei */
+  uint cautionMinimalePrestataire;
 
   /* sender lors de la création du contrat, wallet applicatif : */
   address buy4me;
-  /* vendeur : */
-  address vendeur;
-  /* acheteur : */
-  address acheteur;
-  bool vendeurApprove;
-  bool acheteurApprove;
+  /* Le wallet du prestataire, la personne qui rend service : */
+  address prestataire;
+  /* Le wallet de l'acheteur final, la personne qui paye le service : */
+  address acheteurFinal;
+  /* qui a mis combien : */
+  mapping(address => uint) funds;
+  /* (the default value for a bool is false) : */
+  bool validationAcheteurFinal;
+  /* settlement done : */
+  bool settlementDone;
 
   /* constructeur */
   function Buy4MeContract() {
@@ -19,104 +34,125 @@ contract Buy4MeContract {
 
   /* permissions */
   modifier estBuy4me() {
-    if(buy4me != mmsg.sender) {
-      throw;
-    } else {
-      -;
-    }
+    require(buy4me == msg.sender);
+    _;
   }
-  modifier estVendeur() {
-    if(vendeur != mmsg.sender) {
-      throw;
-    } else {
-      -;
-    }
+  modifier estPrestataire() {
+    require(prestataire == msg.sender);
+    _;
   }
-  modifier estAcheteur() {
-    if(acheteur != mmsg.sender) {
-      throw;
-    } else {
-      -;
-    }
+  modifier estAcheteurFinal() {
+    require(acheteurFinal == msg.sender);
+    _;
+  }
+  modifier estPartiePrenante() {
+    require(prestataire == msg.sender || acheteurFinal == msg.sender);
+    _;
   }
 
-  /* getters */
-  function getvendeur() public constant returns(address) {
-    return vendeur;
+  /* getters et setters pour les paramètres du contrat */
+  function getCommissionBuy4Me() public constant returns(uint returnValue) {
+    return commissionBuy4Me;
   }
-  function getacheteur() public constant returns(address) {
-    return acheteur;
+  function setCommissionBuy4Me(uint comissionBuy4MeParam) public estBuy4me {
+    commissionBuy4Me = comissionBuy4MeParam;
+  }
+  function getPenaliteAnnulationAcheteurFinal() public constant returns(uint returnValue) {
+    return penaliteAnnulationAcheteurFinal;
+  }
+  function setPenaliteAnnulationAcheteurFinal(uint penaliteAnnulationAcheteurFinalParam) public estBuy4me {
+    penaliteAnnulationAcheteurFinal = penaliteAnnulationAcheteurFinalParam;
+  }
+  function getCautionMinimalePrestataire() public constant returns(uint returnValue) {
+    return cautionMinimalePrestataire;
+  }
+  function setCautionMinimalePrestataire(uint cautionMinimalePrestataireParam) public estBuy4me {
+    cautionMinimalePrestataire = cautionMinimalePrestataireParam;
   }
 
   event SetupEvent();
   /* C'est le wallet applicatif qui doit appeler cette méthode */
-  function setup(address vendeurParam, address acheteurParam) estBuy4me {
-    vendeur = vendeurParam;
-    acheteur = acheteurParam;
+  function setup(address prestataireParam, address acheteurFinalParam) estBuy4me {
+    prestataire = prestataireParam;
+    acheteurFinal = acheteurFinalParam;
     SetupEvent();
   }
-  
-  event DepositEvent();
-  /* l'acheteur dépose les fonds */
-  function deposit() estAcheteur {
-    this.balance += msg.value;//TODO KO
-    DepositEvent();
+
+  /* 1. Chaque partie prenante dépose son argent */
+  event DepositEvent(uint depositedAmount, address from);
+  function deposit() estPartiePrenante payable {
+    /* msg.value is in wei */
+    if (msg.value <= 0) throw;
+    funds[msg.sender] += msg.value;
+    DepositEvent(msg.value, msg.sender);
   }
 
-  /* commission (fee) dés que les deux parties ont approuvé */
-  function acheteurApprouve() estAcheteur {
-      acheteurApprove = true;
-      if(vendeurApprove && acheteurApprove) {
-        feeAndPayout();
-      } 
-  }
-  function vendeurApprouve() estVendeur {
-      vendeurApprove = true;
-      if(vendeurApprove && acheteurApprove) {
-        feeAndPayout();
-      } 
+  /* 2. La transaction physique a eu lieu. L'acheteur final valide ou pas */
+  event ValidateEvent();
+  function validate() estAcheteurFinal {
+    validationAcheteurFinal = true;
+    ValidateEvent();
   }
 
-  /* commission puis transfert au vendeur */
-  function feeAndPayout() private {  
-    //0.1% fee
-    if(buy4me.send(this.balance / 1000)) {
-      this.balance -= this.balance / 1000; 
+  /* 3. utile pour le prestataire qui veut vérifier la validation */
+  function getValidationAcheteurFinal() public constant returns(bool returnValue) {
+    return validationAcheteurFinal;
+  }
+
+  /* 4. settlement */
+  event SettleEvent(uint sentAmount, address recipient);
+  function settle() estPartiePrenante {
+
+    if(settlementDone) throw;
+
+    /* pour l'event : */
+    uint sentAmount;
+    address recipient;
+
+    /* utilisé dans les deux cas : */
+    uint montantTransaction = funds[acheteurFinal];
+    settlementDone = true;
+
+    if(validationAcheteurFinal) {
+      /* cas où l'acheteur final a validé */
+      uint cautionPrestataire = funds[prestataire];
+      uint montantApresCommission = cautionPrestataire + montantTransaction - commissionBuy4Me;
+      funds[prestataire] = 0;
+      funds[acheteurFinal] = 0;
+      sentAmount = montantApresCommission;
+      recipient = prestataire;
+      if (!prestataire.send(montantApresCommission)) {
+        // reverting state because send failed
+        funds[prestataire] = cautionPrestataire;
+        funds[acheteurFinal] = montantTransaction;
+        settlementDone = false;
+        sentAmount = 0;
+      }
+    } else {
+      /* cas où l'acheteur final n'a pas validé */
+      uint refund = montantTransaction - penaliteAnnulationAcheteurFinal;
+      funds[acheteurFinal] = penaliteAnnulationAcheteurFinal;
+      sentAmount = refund;
+      recipient = acheteurFinal;
+      if(!acheteurFinal.send(refund)) {
+        // reverting state because send failed
+        funds[acheteurFinal] = montantTransaction;
+        settlementDone = false;
+        sentAmount = 0;
+      }
     }
-    //paye
-    payOut();
+
+    SettleEvent(sentAmount, recipient);
   }
 
-  /* transfert au vendeur */
-  function payOut() private {
-    if(seller.send(this.balance)) {
-      this.balance = 0;
-    }
-  }
-
-  function abort(){
-      if(msg.sender == acheteur) {
-        acheteurApprove = false;
-        if(!vendeurApprove && !acheteurApprove) {
-          refund();
-        } 
-      } 
-      else if (msg.sender == vendeur) {
-        vendeurApprove = false;
-        if(!vendeurApprove && !acheteurApprove) {
-          refund();
-        } 
-      } 
-  }
-
-  function refund() private {
-    if(acheteurApprove == false && vendeurApprove == false) selfdestruct(acheteur);
-    //send money back to recipient if both parties agree contract is void
-  }
-
-  function killContract() internal {
+  /* 5. Buy4Me solde le compte */
+  event KillContractEvent(uint reclaimedFunds);
+  function killContract() estBuy4me {
+      uint remainingBalance = this.balance;
+      //pays buy4me :
       selfdestruct(buy4me);
-      //kills contract and returns funds to acheteur
+      //kills contract and returns funds to buy4me :
+      KillContractEvent(remainingBalance);
   }
 
 }
